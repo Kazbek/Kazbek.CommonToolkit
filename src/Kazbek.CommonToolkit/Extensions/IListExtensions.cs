f3a881f8bb64f9bf6b03398601d2bda10f3fc9dc
Kazbek <kazbek.se@gmail.com>
2025-08-21 15:07:28 +0300

    Метод перемешивания списка

---
+++ src/Kazbek.CommonToolkit/Extensions/IListExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Kazbek.CommonToolkit.Extensions;
+
+public static class IListExtensions
+{
+    public static void Shuffle<T>(this IList<T> list, Random? random = null)
+    {
+        random ??= Random.Shared;
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = random.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+}
--- src/Kazbek.CommonToolkit/Extensions/ListExtensions.cs
+++ src/Kazbek.CommonToolkit/Extensions/ListExtensions.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Kazbek.CommonToolkit.Extensions
+namespace Kazbek.CommonToolkit.Extensions;
+
+public static class ListExtensions
 {
-    public static class ListExtensions
+    public static void SynchronizeWith<TSource,TTarget>(this List<TSource> source, IList<TTarget> target, Func<TSource, TTarget,bool> compareFunction, Func<TTarget, TSource> converter)
     {
-        public static void SynchronizeWith<TSource,TTarget>(this List<TSource> source, IList<TTarget> target, Func<TSource, TTarget,bool> compareFunction, Func<TTarget, TSource> converter)
-        {
-            source.RemoveAll(s => !target.Any(t => compareFunction(s, t)));
-            source.AddRange(target.Where(t => source.All(s => !compareFunction(s, t))).Select(converter).ToList());
-        }
+        source.RemoveAll(s => !target.Any(t => compareFunction(s, t)));
+        source.AddRange(target.Where(t => source.All(s => !compareFunction(s, t))).Select(converter).ToList());
     }
+
 }
